e7e89ae5ecd1bead70cb759e65210ecaf1a8099d
witeseb <tanatanee.wbst@mail.kmutt.ac.th>
2025-02-09 21:02:21 +0700

    Initial setup commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home(){
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3x1 font-bold">
+          portfolio start
+        </h1>
+      </div>
+    </section>
+  )
+}
